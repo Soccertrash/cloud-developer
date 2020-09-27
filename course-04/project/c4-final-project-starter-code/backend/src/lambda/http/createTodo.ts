510ca6d5c904dd61104d682fa0dfed4872545c83
Manfred Pauli <manfred.pauli@gmail.com>
2020-09-27 12:34:18 +0200

    Implement get request

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,46 @@
 import 'source-map-support/register'
+import * as uuid from 'uuid'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import {APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult} from 'aws-lambda'
 
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import {CreateTodoRequest} from '../../requests/CreateTodoRequest'
+import {TodoAccess} from "../../datalayer/todoAccess";
+import {createLogger} from "../../utils/logger";
+import {TodoItem} from "../../models/TodoItem";
+//import {parseUserId} from "../../auth/utils";
+
+const todoAccess = new TodoAccess();
+const logger = createLogger('createTodo');
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+    logger.debug('Process createTodo', event);
+
+    const newTodo: CreateTodoRequest = JSON.parse(event.body)
+
+    const todoId = uuid.v4()
+    const userId = 'test';//parseUserId();
+
+    const todoItem: TodoItem = {
+        userId: userId,
+        todoId: todoId,
+        createdAt: new Date().toISOString(),
+        name: newTodo.name,
+        dueDate: newTodo.dueDate,
+        done: false
+
+    }
+
+    const result = await todoAccess.createTodo(todoItem);
+
+    return {
+        statusCode: 201,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({
+            result
+        })
+    }
 }
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,25 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler} from 'aws-lambda'
+import {TodoAccess} from "../../datalayer/todoAccess";
+import {createLogger} from "../../utils/logger";
+
+const todoAccess = new TodoAccess();
+const logger = createLogger('getTodos');
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+
+    logger.debug('Processing event:', event);
+
+    const items = await todoAccess.getAllTodos();
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            items: items
+        })
+    }
 }
