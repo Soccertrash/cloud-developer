3765e0cc55c581e656197651fcc0db60cc871d2c
Manfred Pauli <manfred.pauli@gmail.com>
2020-10-04 13:48:49 +0200

    add secondary index

--- course-04/project/c4-final-project-starter-code/backend/src/datalayer/todoAccess.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/datalayer/todoAccess.ts
@@ -1,108 +1,116 @@
 import * as AWS from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
-import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+import {DocumentClient} from 'aws-sdk/clients/dynamodb'
+import {TodoItem} from '../models/TodoItem'
+import {createLogger} from "../utils/logger";
+import {UpdateTodoRequest} from "../requests/UpdateTodoRequest";
 
 const XAWS = AWSXRay.captureAWS(AWS)
-
-import { TodoItem } from '../models/TodoItem'
-import { createLogger } from "../utils/logger";
-import { UpdateTodoRequest } from "../requests/UpdateTodoRequest";
 
 const logger = createLogger('datalayer');
 
 
 export class TodoAccess {
 
-  constructor(
-    private readonly docClient: DocumentClient = createDynamoDBClient(),
-    private readonly todoTable = process.env.TODO_TABLE) {
-  }
+    constructor(
+        private readonly docClient: DocumentClient = createDynamoDBClient(),
+        private readonly todoTable = process.env.TODO_TABLE,
+        private readonly todoUserIdIndex = process.env.TODO_USER_ID_INDEX) {
+    }
 
-  async getAllTodos(): Promise<TodoItem[]> {
-    console.log('Getting all items')
-
-    const result = await this.docClient.scan({
-      TableName: this.todoTable
-    }).promise()
-
-    const items = result.Items
-    return items as TodoItem[]
-  }
-
-  async createTodo(todo: TodoItem): Promise<TodoItem> {
-    logger.debug("CreateTodo", todo)
-    await this.docClient.put({
-      TableName: this.todoTable,
-      Item: todo
-    }).promise()
-    logger.debug("Done", todo)
-
-    return todo
-  }
+    async getAllTodos(userId: string): Promise<TodoItem[]> {
+        logger.debug(`Get all todos for user ${userId}`);
 
 
-  async updateTodo(todoId: string, todo: UpdateTodoRequest) {
-    logger.debug(`UpdateTodo (${todoId})`, todo)
+        const result = await this.docClient
+            .query({
+                TableName: this.todoTable,
+                IndexName: this.todoUserIdIndex,
+                KeyConditionExpression: 'userId = :userIdKey',
+                ExpressionAttributeValues: {
+                    ':userIdKey': userId
+                }
+            })
+            .promise()
 
-    await this.docClient.update(
-      {
-        TableName: this.todoTable,
-        Key: { "todoId": todoId },
-        ExpressionAttributeNames: { "#nameAlt": "name" },
-        UpdateExpression: "set #nameAlt = :n, dueDate = :d, done = :o",
-        ExpressionAttributeValues: {
-          ":n": todo.name,
-          ":d": todo.dueDate,
-          ":o": todo.done
-        },
-        ReturnValues: "UPDATED_NEW"
+        const items = result.Items
+        return items as TodoItem[]
+    }
 
-      }
-    ).promise();
+    async createTodo(todo: TodoItem): Promise<TodoItem> {
+        logger.debug("CreateTodo", todo)
+        await this.docClient.put({
+            TableName: this.todoTable,
+            Item: todo
+        }).promise()
+        logger.debug("Done", todo)
 
-  }
+        return todo
+    }
 
-  async setUpdateUrl(todoId: string, url: string) {
-    logger.debug(`setUpdateUrl (${todoId}, ${url})`)
 
-    await this.docClient.update(
-      {
-        TableName: this.todoTable,
-        Key: { "todoId": todoId },
-        UpdateExpression: "set attachmentUrl = :u",
-        ExpressionAttributeValues: {
-          ":u": url
-        },
-        ReturnValues: "UPDATED_NEW"
+    async updateTodo(todoId: string, todo: UpdateTodoRequest) {
+        logger.debug(`UpdateTodo (${todoId})`, todo)
 
-      }
-    ).promise();
+        await this.docClient.update(
+            {
+                TableName: this.todoTable,
+                Key: {"todoId": todoId},
+                ExpressionAttributeNames: {"#nameAlt": "name"},
+                UpdateExpression: "set #nameAlt = :n, dueDate = :d, done = :o",
+                ExpressionAttributeValues: {
+                    ":n": todo.name,
+                    ":d": todo.dueDate,
+                    ":o": todo.done
+                },
+                ReturnValues: "UPDATED_NEW"
 
-  }
+            }
+        ).promise();
 
-  async deleteTodo(todoId: string) {
-    logger.debug(`Delete (${todoId}`)
+    }
 
-    await this.docClient.delete(
-      {
-        TableName: this.todoTable,
-        Key: { "todoId": todoId }
-      }
-    ).promise();
+    async setUpdateUrl(todoId: string, url: string) {
+        logger.debug(`setUpdateUrl (${todoId}, ${url})`)
 
-  }
+        await this.docClient.update(
+            {
+                TableName: this.todoTable,
+                Key: {"todoId": todoId},
+                UpdateExpression: "set attachmentUrl = :u",
+                ExpressionAttributeValues: {
+                    ":u": url
+                },
+                ReturnValues: "UPDATED_NEW"
+
+            }
+        ).promise();
+
+    }
+
+    async deleteTodo(todoId: string) {
+        logger.debug(`Delete (${todoId}`)
+
+        await this.docClient.delete(
+            {
+                TableName: this.todoTable,
+                Key: {"todoId": todoId}
+            }
+        ).promise();
+
+    }
 
 
 }
 
 function createDynamoDBClient() {
-  if (process.env.IS_OFFLINE) {
-    console.log('Creating a local DynamoDB instance')
-    return new XAWS.DynamoDB.DocumentClient({
-      region: 'localhost',
-      endpoint: 'http://localhost:8000'
-    })
-  }
+    if (process.env.IS_OFFLINE) {
+        console.log('Creating a local DynamoDB instance')
+        return new XAWS.DynamoDB.DocumentClient({
+            region: 'localhost',
+            endpoint: 'http://localhost:8000'
+        })
+    }
 
-  return new XAWS.DynamoDB.DocumentClient()
+    return new XAWS.DynamoDB.DocumentClient()
 }
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register'
 import {APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult} from 'aws-lambda'
 import {TodoAccess} from "../../datalayer/todoAccess";
 import {createLogger} from "../../utils/logger";
+import {getUserId} from "../utils";
 
 const todoAccess = new TodoAccess();
 const logger = createLogger('getTodos');
@@ -11,7 +12,9 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
 
     logger.debug('Processing event:', event);
 
-    const items = await todoAccess.getAllTodos();
+    const userId = getUserId(event);
+
+    const items = await todoAccess.getAllTodos(userId);
 
     return {
         statusCode: 200,
